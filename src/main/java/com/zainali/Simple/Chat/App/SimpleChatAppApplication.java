4402ab73f53fdcaac231303515fecf0c96fd7311
Zain <me@zainali.xyz>
2020-07-21 14:02:52 -0400

    initial commit

---
+++ src/main/java/com/zainali/Simple/Chat/App/SimpleChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.zainali.Simple.Chat.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zainali/Simple/Chat/App/SimpleChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zainali.Simple.Chat.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
