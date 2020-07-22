9eb9cccb3912d72ae7ea607006890909fc0e12cb
Zain <me@zainali.xyz>
2020-07-22 16:47:03 -0400

    Fixed message effects feature

--- src/main/java/com/zainali/Simple/Chat/App/ChatForm.java
+++ src/main/java/com/zainali/Simple/Chat/App/ChatForm.java
@@ -3,6 +3,7 @@ package com.zainali.Simple.Chat.App;
 public class ChatForm {
     private String message;
     private String userName;
+    private String messageEffect = "";
 
     public void setMessage(String message){
         this.message = message;
@@ -12,11 +13,31 @@ public class ChatForm {
         this.userName = userName;
     }
 
+    public void setMessageEffect(String messageEffect){
+        this.messageEffect = messageEffect;
+    }
+
     public String getMessage(){
+        if(messageEffect.equals("shout")){
+            return message.toUpperCase();
+        }else if(messageEffect.equals("whisper")){
+            return message.toLowerCase();
+        }
+
         return message;
     }
 
     public String getUserName(){
+        if(messageEffect.equals("shout")){
+            return userName.toUpperCase();
+        }else if(messageEffect.equals("whisper")){
+            return userName.toLowerCase();
+        }
+
         return userName;
+    }
+
+    public String getMessageEffect(){
+        return messageEffect;
     }
 }
