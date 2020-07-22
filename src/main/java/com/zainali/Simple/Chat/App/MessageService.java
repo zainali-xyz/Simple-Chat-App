ed01366945b36ec45a09c295c98fa0db6275ac4e
Zain <me@zainali.xyz>
2020-07-21 21:28:21 -0400

    Created a service for the controller interaction for updating data

--- src/main/java/com/zainali/Simple/Chat/App/MessageService.java
+++ src/main/java/com/zainali/Simple/Chat/App/MessageService.java
@@ -1,4 +1,23 @@
 package com.zainali.Simple.Chat.App;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageService {
+    List<ChatMessage> messages;
+
+    @PostConstruct
+    public void postConstruct(){
+        this.messages = new ArrayList<>();
+    }
+
+    public void addMessage(ChatMessage chatMessage){
+        messages.add(chatMessage);
+    }
+
+    public List<ChatMessage> getMessages() {
+        return new ArrayList<>(this.messages);
+    }
+
 }
