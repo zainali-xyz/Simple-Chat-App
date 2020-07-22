0dabda0a3fb7282753deaeb4e43baa4e27cd6f7a
Zain <me@zainali.xyz>
2020-07-21 21:20:10 -0400

    Updated the class for a centralized place to store all message info

--- src/main/java/com/zainali/Simple/Chat/App/ChatMessage.java
+++ src/main/java/com/zainali/Simple/Chat/App/ChatMessage.java
@@ -1,4 +1,30 @@
 package com.zainali.Simple.Chat.App;
 
+import com.sun.jdi.request.StepRequest;
+
 public class ChatMessage {
+    String userName;
+    String message;
+
+    public ChatMessage(String userName, String message){
+        this.userName = userName;
+        this.message = message;
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
 }
