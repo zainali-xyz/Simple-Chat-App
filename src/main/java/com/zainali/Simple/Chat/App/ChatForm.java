faa4a02037325e91ee1e586548f55be550991bd1
Zain <me@zainali.xyz>
2020-07-21 21:11:38 -0400

    Updated class to store form data

--- src/main/java/com/zainali/Simple/Chat/App/ChatForm.java
+++ src/main/java/com/zainali/Simple/Chat/App/ChatForm.java
@@ -1,4 +1,22 @@
 package com.zainali.Simple.Chat.App;
 
 public class ChatForm {
+    private String message;
+    private String userName;
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
 }
