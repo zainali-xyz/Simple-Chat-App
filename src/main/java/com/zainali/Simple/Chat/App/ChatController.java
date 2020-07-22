08bb134703b293a818d471bf9735cffdd077c93b
Zain <me@zainali.xyz>
2020-07-21 22:21:07 -0400

    Updated controller to process the http requests. Still need to debug the text effects

--- src/main/java/com/zainali/Simple/Chat/App/ChatController.java
+++ src/main/java/com/zainali/Simple/Chat/App/ChatController.java
@@ -1,9 +1,33 @@
 package com.zainali.Simple.Chat.App;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ChatController {
+    private MessageService messageService;
 
+    public ChatController(MessageService messageService){
+        this.messageService = messageService;
+    }
+
+    @GetMapping("/chat")
+    public String getChat(@ModelAttribute("newChat") ChatForm newChat, Model model){
+        model.addAttribute("chats", this.messageService.getMessages());
+        return "chat";
+    }
+
+    @PostMapping("/chat")
+    public String addChat(@ModelAttribute("newChat") ChatForm chatForm, Model model) {
+        ChatMessage chat = new ChatMessage(chatForm.getUserName(), chatForm.getMessage());
+        messageService.addMessage(chat);
+        model.addAttribute("chats", messageService.getMessages());
+        chatForm.setMessage("");
+        chatForm.setUserName("");
+        return "chat";
+    }
 
 }
