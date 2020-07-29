541961e75bff520806df109fa3380a73519929df
Zain <me@zainali.xyz>
2020-07-29 12:05:27 -0400

    Matched the messages with the database table

--- src/main/java/com/zainali/Simple/Chat/App/Model/ChatMessage.java
+++ src/main/java/com/zainali/Simple/Chat/App/Model/ChatMessage.java
@@ -4,22 +4,30 @@ import com.sun.jdi.request.StepRequest;
 
 public class ChatMessage {
     private String userName;
-    private String message;
+    private String messageText;
+    private Integer messageid;
 
     public void setUserName(String userName){
         this.userName = userName;
     }
 
-    public void setMessage(String message){
-        this.message = message;
+    public void setMessageText(String messageText){
+        this.messageText = messageText;
     }
 
     public String getUserName(){
         return userName;
     }
 
-    public String getMessage(){
-        return message;
+    public String getMessageText(){
+        return messageText;
     }
 
+    public Integer getMessageid() {
+        return messageid;
+    }
+
+    public void setMessageid(Integer messageid) {
+        this.messageid = messageid;
+    }
 }
