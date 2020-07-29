e0828d19526449cffee75bac6f5d781f9301f875
Zain <me@zainali.xyz>
2020-07-29 11:59:43 -0400

    Added User class that matches the data of the database table

---
+++ src/main/java/com/zainali/Simple/Chat/App/Model/User.java
@@ -0,0 +1,58 @@
+package com.zainali.Simple.Chat.App.Model;
+
+public class User {
+    Integer userid;
+    String username;
+    String VARCHAR;
+    String password;
+    String firstname;
+    String lastname;
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getVARCHAR() {
+        return VARCHAR;
+    }
+
+    public void setVARCHAR(String VARCHAR) {
+        this.VARCHAR = VARCHAR;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
